e560f287e204bba1dd1fa5f6d318dc3af674fb22
Nelson Lee <leelhn2345@outlook.com>
2024-09-20 01:39:00 +0800

    font + nginx changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,10 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <div className="text-4xl">
+        Whereas disregard and contempt for human rights have resulted
+      </div>
+      <h1 className="text-4xl font-extrabold">hello</h1>
     </div>
   );
 }
